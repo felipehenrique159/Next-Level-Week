5a2e615e553c6de18f7d05312469ae0588d69c1c
felipehenrique159 <felipecod6@hotmail.com>
2020-06-04 01:53:15 -0300

    instalação e config cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import routes from './routes';
 import path from 'path';
+import cors from 'cors';
 import connection from './database/connection';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json()) 
 app.use(routes)
 
