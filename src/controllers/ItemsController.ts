28a62e12a32701b7d05e64ff42ccad50b83cedf9
felipehenrique159 <felipecod6@hotmail.com>
2020-06-04 01:19:19 -0300

    criado controllers e estruturado rotas

---
+++ src/controllers/ItemsController.ts
@@ -0,0 +1,21 @@
+import {Request,Response} from 'express';
+import connection from '../database/connection';
+class ItemsController{
+    
+    async index(req: Request,res:Response){
+        const items = await connection('items').select('*')
+    
+        const serializedItems = items.map(item => {  //map para percorrer array
+            return {
+                id: item.id,
+                title: item.title,
+                image_url: `http://localhost:3000/uploads/${item.image}`
+            }
+        })
+    
+        return res.json(serializedItems);
+    }
+}
+
+export default ItemsController
+
--- src/routes.ts
+++ src/routes.ts
@@ -1,69 +1,13 @@
 import express, { response } from 'express';
-import connection from './database/connection';
-
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items',async (req,res)=>{
-   
-    const items = await connection('items').select('*')
-
-    const serializedItems = items.map(item =>{  //map para percorrer array
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3000/uploads/${item.image}`
-        }
-    })
-
-    return res.json(serializedItems);
-
- })
-
- routes.post('/points', async(req,res)=>{
-//    console.log(req.body)
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = req.body;
-
-    const trx = await connection.transaction(); //para transação, caso erro é executado um rollback
-
-    const insertedIds = await trx('points').insert({
-     
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        
-        
-    });
-
-    const pointItems = items.map((item_id: number) =>{
-        return{
-            item_id,
-            point_id:insertedIds[0]
-        }
-    })
-
-    const point_id = insertedIds[0];
-    
-    await trx('point_items').insert(pointItems);
-    return res.json({
-        success:true
-    })
-
- })
-
+routes.get('/items', itemsController.index)
+routes.get('/points/:id', pointsController.show)
+routes.post('/points', pointsController.create)
 
 export default routes;
