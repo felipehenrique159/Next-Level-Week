bee06a6b4c18fc38d8079bb7030d9d45255e5cf4
felipehenrique159 <felipecod6@hotmail.com>
2020-06-19 12:27:28 -0300

    Validação de dados

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,15 @@ import routes from './routes';
 import path from 'path';
 import cors from 'cors';
 import connection from './database/connection';
+import {errors} from 'celebrate'
 
 const app = express();
 
 app.use(cors())
 app.use(express.json()) 
 app.use(routes)
+app.use(errors())
+app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')))  //para acessar dados estaticos
 
 app.listen(3000)
-
-app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')))  //para acessar dados estaticos
 
